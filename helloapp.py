732f6d114779630e9f96d32b5c55adc41f2e6371
arlinndi9 <arlinndi9@gmail.com>
2022-11-26 14:20:04 +0100

    update2

--- helloapp.py
+++ helloapp.py
@@ -1,7 +1,9 @@
 import streamlit as st
-
+from datetime import date
 st.title('streamlit')
 st.text('hello')
 st.subheader('helloapp')
+today=date.today()
+st.write(today)
 st.text('github')
 st.header('my name is arlind ')
