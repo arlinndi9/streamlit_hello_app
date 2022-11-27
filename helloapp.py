d80f636033bdd0961e1a3eef53d6ba291e7064df
arlinndi9 <arlinndi9@gmail.com>
2022-11-28 00:43:05 +0100

    update5

--- helloapp.py
+++ helloapp.py
@@ -4,11 +4,11 @@ st.title('streamlit')
 st.text('hello')
 st.subheader('helloapp')
 today=date.today()
-f_date = date(2022,11,26)
+f_date = today
 l_date = date(2023, 4, 3)
 delta = l_date - f_date
 
 st.write(today)
 st.text('github')
 st.header('my name is arlind ')
-st.title(f'ditelindja ime eshte edhe{delta}')
+st.title(f'ditelindja ime eshte edhe {delta.days}')
